b365b1127fe6835a4bfbe8d10548960e86cf6530
Pavlo <teenitclub@gmail.com>
2024-05-06 20:56:38 +0300

    Fix calendar

--- serve/user/get-cal-month.php
+++ serve/user/get-cal-month.php
@@ -1,25 +1,35 @@
 <?php
 require_once '../config.php';
+
 $data = json_decode(file_get_contents('php://input'));
 $id = $data->id;
 $month = $data->month;
 $year = $data->year;
 $mas = [];
-    $msql = "SELECT * FROM calendar WHERE (user_id=$id OR user_id=0) AND month=$month OR (year=$year || user_id=0)";
+$monthplus = $month + 1;
+$monthminus = $month - 1;
+$msql = "SELECT * FROM calendar WHERE (user_id=$id OR user_id=0) AND (((month=$month OR month=$monthplus OR month=$monthplus) AND year=$year) OR (every_year=1) OR (meta_key='happyCase'))";
 
-   $result = mysqli_query($conn, $msql);
-   
-   while ($res = mysqli_fetch_array($result)) {
+$result = mysqli_query($conn, $msql);
 
-    $user = new StdClass();
-    $user->{"id"} = $res['id'];
-    $user->{"value"} = json_decode($res['meta_value']);
-    $user->{"date"} = $res['date'];
-    $user->{'day'} = $res['day'];
-    $user->{'month'} = $res['month'];
-    $user->{'key'} = $res['meta_key'];
-    $mas[] = $user;
- }
+if ($result) {
+    while ($res = mysqli_fetch_array($result)) {
+        $user = new StdClass();
+        $user->{"id"} = $res['id'];
+        $user->{"value"} = json_decode($res['meta_value']);
+        $user->{"date"} = $res['date'];
+        $user->{'day'} = $res['day'];
+        $user->{'month'} = $res['month'];
+        $user->{'key'} = $res['meta_key'];
+        $mas[] = $user;
+    }
+
     echo json_encode($mas);
-  mysqli_close($conn);
-  exit;
+} else {
+    // Обробка помилок, якщо запит не вдалося виконати
+    echo json_encode(["error" => mysqli_error($conn)]);
+}
+
+mysqli_close($conn);
+exit;
+?>
