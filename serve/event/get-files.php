5ed0e9fef2f44faa041a9a1044a7a04f81abf1ec
Pavlo <teenitclub@gmail.com>
2024-06-11 03:20:44 +0300

    Fix get-files for event

--- serve/event/get-files.php
+++ serve/event/get-files.php
@@ -4,7 +4,6 @@ require_once '../config.php';
 $data = json_decode(file_get_contents('php://input'));
 
 $mike = false;
-if(!checkRight($data->id, 'getEventFile', $token, true)) exit;
 $mas = [];
 $eventID = $data->eventID;
 $key = $data->key;
@@ -12,10 +11,11 @@ $msql = "SELECT * FROM eventsmeta WHERE event_id='$eventID' AND meta_key='$key'"
 $event = mysqli_query($conn, $msql);
 
 while ($res = mysqli_fetch_array($event)) {
-    $obj->{'docs'} = json_decode($res['meta_value'], JSON_UNESCAPED_UNICODE);
+    $obj = new StdClass();
     $obj->{'id'} = $res['id'];
-    echo json_encode($obj, JSON_UNESCAPED_UNICODE);
-    exit;
+    $obj->{'fileInfo'} = json_decode($res['meta_value'], JSON_UNESCAPED_UNICODE);
+    $mas[] = $obj;
+   // echo json_encode($obj, JSON_UNESCAPED_UNICODE);
 }
 
 
