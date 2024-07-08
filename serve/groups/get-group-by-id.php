16b251652c41580375a77be53ca70206f9451f05
Pavlo <teenitclub@gmail.com>
2024-07-09 01:05:34 +0300

    fix back get group by id

--- serve/groups/get-group-by-id.php
+++ serve/groups/get-group-by-id.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../config.php';
-
+require_once '../functions.php';
 // Отримання даних з фронту
 $data = json_decode(file_get_contents('php://input'), true);
 
@@ -13,6 +13,7 @@ if (isset($data['group_id'])) {
         SELECT 
             g.*, 
             g.id as group_id,
+            g.name as groupName,
             gc.*, 
             gc.id as group_connect_id,
             cn.*, 
@@ -30,14 +31,33 @@ if (isset($data['group_id'])) {
         
         // Отримання результатів запиту
         $result = $stmt->get_result();
-        $response = [];
-
+        
+        $members = [];
         while ($row = $result->fetch_assoc()) {
-            $response[] = $row;
+            $members[] = [
+                'case_id' => $row['case_id'],
+                'name' => $row['name'],
+                'why' => $row['why'],
+                'phone1' => decryptData($row['phone1'], $key),
+                'phone2' => decryptData($row['phone2'], $key)
+            ];
+            $response = [
+                'groupName' => $row['groupName'],
+                'group_id' => $row['group_id'],
+                'groupDescription' => $row['description'],
+                'groupColor' => $row['color'],
+                'groupCategories' => json_decode($row['categories']),
+                'groupDateCreated' => $row['date_created']
+            ];
         }
+        $res = [
+            'group' => $response,
+            'members' => $members
+        ];
+     
 
         // Виведення результату у форматі JSON
-        echo json_encode($response);
+        echo json_encode($res);
     } else {
         // Помилка підготовки запиту
         echo json_encode(['error' => 'Failed to prepare the SQL query.', 'status' => false]);
